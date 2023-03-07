0674ed7d7cc9393d79c00cdbfef6ea1d3f4fae10
ltphuongunited <68008652+ltphuongunited@users.noreply.github.com>
2023-03-07 13:19:08 +0700

    update

--- script.sh
+++ script.sh
@@ -1,36 +1,43 @@
 #!/bin/bash
 
-# Get a list of all directories
-dirs=$(ls -d */)
+# Tạo thư mục cha mới
+mkdir new_parent_directory
 
-# Loop through each directory
-for dir in $dirs; do
-  # Change into the directory
-  cd "$dir"
+# Lặp qua tất cả các thư mục trong thư mục cha
+for directory in asl_alphabet_train/*; do
+    # Tạo thư mục con mới trong thư mục cha mới
+    mkdir new_parent_directory/$(basename "$directory")
 
-  # Get a list of all files
-  files=$(ls -1)
+    # Lấy tất cả các file trong thư mục hiện tại
+    files=($directory/*)
 
-  # Counter for keeping track of the number of files processed
-  counter=0
+    # Đảo ngẫu nhiên danh sách các file
+    shuf_files=($(shuf -e "${files[@]}"))
 
-  # Loop through each file
-  for file in $files; do
-    # Increment the counter
-    counter=$((counter + 1))
+    # Tính toán số lượng file cần di chuyển vào thư mục mới
+    num_files=$(( ${#shuf_files[@]} ))
 
-    # Check if the counter is a multiple of 15
-    if [ $((counter % 15)) -eq 0 ]; then
-      # Keep the file
-      continue
+    # Tính toán số lượng file cần di chuyển vào thư mục mới
+    num_files_to_move=$(( $num_files * 2 / 10 ))
+
+    # Tạo một số ngẫu nhiên từ 1 đến 10
+    random_number=$((RANDOM%10+1))
+
+    # Di chuyển các file vào thư mục mới hoặc thư mục hiện tại dựa trên số ngẫu nhiên
+    if [[ "$random_number" -le 8 ]]; then
+        for (( i=0; i<$num_files_to_move; i++ )); do
+            mv "${shuf_files[$i]}" "new_parent_directory/$(basename "$directory")"
+        done
+        for (( i=$num_files_to_move; i<$num_files; i++ )); do
+            mv "${shuf_files[$i]}" "$directory"
+        done
+    else
+        for (( i=0; i<$num_files_to_move; i++ )); do
+            mv "${shuf_files[$i]}" "$directory"
+        done
+        for (( i=$num_files_to_move; i<$num_files; i++ )); do
+            mv "${shuf_files[$i]}" "new_parent_directory/$(basename "$directory")"
+        done
     fi
-
-    # Delete the file
-    rm "$file"
-  done
-
-  # Change back to the parent directory
-  cd ..
 done
-
 
